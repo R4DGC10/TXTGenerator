2dd80872010150d9dfd159ee891594c1aead1141
R4DGC10 <132533968+R4DGC10@users.noreply.github.com>
2023-07-09 20:21:19 -0600

    Add files via upload

--- TXTGen.py
+++ TXTGen.py
@@ -4,44 +4,75 @@ import time
 from tkinter import *
 from tkinter import ttk
 
+from tkinter import messagebox
+
+
+
+#Icono del Taskbar
+import ctypes
+myappid = 'mycompany.myproduct.subproduct.version' # arbitrary string
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+
+
 #Creacion de Ventana y Dimensiones
 window = tkinter.Tk()
 window.title("TXT Creator")
+window.resizable(False, False)
 window.iconbitmap(r"C:\Users\cepc2\OneDrive\Desktop\Projects\Python\TXTGen\icono.ico")
 window.geometry("800x600")
 
-title_main = tkinter.Label(window, text = ".txt file Generator", bg = "green")
+#Contenido
+title_main = tkinter.Label(window, text = ".txt file Generator", font = "arial", bg = "green")
 title_main.pack(fill = tkinter.X)
 
-label = tkinter.Label(window, text = "Ingrese el nombre del archivo a generar")
+label = tkinter.Label(window, text = "Ingrese el nombre del archivo a generar", font = "arial")
 label.pack()
 
-name_input = tkinter.Entry(window)
+name_input = tkinter.Entry(window,  font = "arial")
 name_input.pack()
 
-label2 = tkinter.Label(window, text = "Default: Escritorio")
-label2.pack()
+while not name_input:
+    msg = tkinter.Label(window,text = "Por favor rellene esta casilla: Nombre de Archivo!")
+    name_input = tkinter.Entry(window)
+    name_input.pack()
+
+#Pop Up al intentar cerrar
+def on_close():
+    if messagebox.askokcancel("Saliendo", "¿Está seguro de que desea salir del programa?"):
+        window.destroy()
+window.protocol("WM_DELETE_WINDOW", on_close)
+
 
 #Funcion define ruta y nombre del archivo.txt
+#Compureba que haya un nombre escrito para el archivo
+
 def filename():
     name_doc = name_input.get()
-    print(name_doc)
-    file_name = name_input.get()
-    file_path = r"C:\Users\cepc2\OneDrive\Desktop"
-    create_file(file_path, file_name)
+    if not name_doc:
+        msg = tkinter.Label(window,text = "Por favor rellene esta casilla: Nombre de Archivo!", font = "arial", bg = "red")
+        msg.pack(fill = tkinter.X)
+        msg.pack()
+        window.after(1500, msg.destroy) 
+    else:
+        print(name_doc)
+        file_name = name_input.get()
+        file_path = r"C:\Users\cepc2\OneDrive\Desktop"
+        create_file(file_path, file_name)
+
     
 
 Confirm1_button = tkinter.Button(window, text = "Confirmar", command = filename)
 Confirm1_button.pack()
 
 #Obtiene Hora
-current = time.strftime("%Y:%H:%M:%S", time.localtime())
+current = time.strftime("%D:%Y:%H:%M:%S", time.localtime())
 print(current)
 
 def create_file(file_path, file_name):
     generated_time = current
     file_content = "Archivo Generado via Python/tkinter => " + file_name 
-    file_c2 = "Date => " + generated_time 
+    file_c2 = "Date =>   " + generated_time 
     
     with open(os.path.join(file_path, file_name), 'w' ) as file:
         file.write(file_content +"\n")
