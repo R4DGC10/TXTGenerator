d2370905a0bcd6703447a3e63bab99c61b0b44a9
Carlo Pacheco <cpachecocerdas@gmail.com>
2023-08-27 16:30:55 -0600

    #Added New Entry box for customizing content from generated file

--- TXTGen.py
+++ TXTGen.py
@@ -36,6 +36,8 @@ title_main.pack(fill = tkinter.X)
 label = tkinter.Label(window, text = "Ingrese el nombre del archivo a generar", font = "arial")
 label.pack()
 
+
+
 #PopUp  (About Button)
 def pop_window():
    
@@ -46,25 +48,43 @@ def pop_window():
     AboutWindow.geometry("200x200")
     
     about_msg = tkinter.Label(AboutWindow, text="Developed by cpacheco_c 2023")
-    about_msg2 = tkinter.Label(AboutWindow, text="tk")
+    about_msg2 = tkinter.Label(AboutWindow, text="Using Tkinter Library in Python v3.11.4")
+    about_msg2 = tkinter.Label(AboutWindow, text="Non comercial Software ")
     about_msg.pack()
     about_msg2.pack()
     button = tk.Button(AboutWindow, text="Cerrar", command=AboutWindow.destroy)
     button.pack(pady=5)
 
+def contact_dev():
+    AboutWindow = Toplevel()
+    AboutWindow.title("Contacta al desarrollador")
+    AboutWindow.resizable(False,False)
+    AboutWindow.iconbitmap(r"C:\Users\cepc2\OneDrive\Desktop\Projects\CodePy\TXTGen\TXTGenerator\icono_new.ico")
+    AboutWindow.geometry("500x300")
 
-    
+    contact_msg = tkinter.Label(AboutWindow, text = "Contacto Comercial")
+    contact_msg2= tkinter.Label(AboutWindow, text="c4dgr10xp@protonmail.com")
+    contact_msg.pack()
+    contact_msg2.pack()
+    button = tk.Button(AboutWindow, text="Cerrar", command=AboutWindow.destroy)
+    button.pack(pady=5)
+
+
+
 
 
 credits_popup = tkinter.Button(window, text = "Acerca de", command = pop_window)
 credits_popup.pack(side="bottom")
 
-label = tkinter.Label(window, text = "cpacheco_c", font = "arial")
-label.pack(side = "bottom")
+hide_popup = tkinter.Button(window, text = "Contacta al desarrollador", command =  contact_dev)
+hide_popup.pack(side="bottom")
+
 
 
 name_input = tkinter.Entry(window,  font = "arial")
 name_input.pack()
+
+
 
 #Ruta de Guardado Custom
 label = tkinter.Label(window, text = "Ruta de Guardado", font = "arial")
@@ -72,6 +92,13 @@ label.pack()
 
 file_input = tkinter.Entry(window)
 file_input.pack()
+
+#Modificar Contenido
+label_editor = tkinter.Label(window, text = "Modificar contenido del archivo", font = "arial")
+label_editor.pack()
+
+label_editor_input = tkinter.Entry(window)
+label_editor_input.pack()
 
 
 #Pop Up al intentar cerrar
@@ -88,6 +115,7 @@ def filename():
     #Re-Declaracion de variables para ser utilizadas localmente en la funcion filename()
     name_doc = name_input.get()
     file_path = file_input.get()
+    mod_txt = label_editor_input.get()
     
     if not name_doc:
         msg = tkinter.Label(window,text = "Por favor rellene casilla => Nombre del Archivo!", font = "arial", bg = "red")
@@ -125,11 +153,15 @@ def create_file(file_path, file_name):
     generated_time = current
     file_content = "Archivo Generado mediante TXTGenerator => " + file_name 
     file_c2 = "Fecha de Creacion =>   " + generated_time 
+    file_input = label_editor_input.get()
+    file_blank = " "
     
     with open(os.path.join(file_path, file_name), 'w' ) as file:
         file.write(file_content +"\n")
         file.write(file_c2+"\n")
         file.write("TXTGenerator_cpacheco_c_2023")
+        file.write(file_blank+"\n")
+        file.write(file_input)
 
 
 #Añadir imagen, requisito pip install pillow
